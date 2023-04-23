53858cec4df00064d0408f1c165f8870268a5b5f
Beimy <arbm159357@gmail.com>
2023-04-23 14:56:02 -0400

    changed to next.js

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html>
+            <main>
+                <nav>
+                    <Link href="/">
+                        Home
+                    </Link>
+                    <Link href="media">
+                        Media
+                    </Link>
+                    <Link href="merch">
+                        Merch
+                    </Link>
+                    <Link href="shows">
+                        Shows
+                    </Link>
+                </nav>
+                {children}
+            </main>
+        </html>
+    )
+}
---
+++ app/shows/page.tsx
@@ -0,0 +1,7 @@
+export default async function ShowsPage() {
+    return(
+        <div>
+            <h1>Shows</h1>
+        </div>
+    )
+}
